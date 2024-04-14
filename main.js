a0e7842baca00364ab7b81524be0b81a5e97efae
Mustafa-Ahmed-Saad <mustafasaad19970@gmail.com>
2024-04-15 01:49:03 +0200

    optimization code

--- main.js
+++ main.js
@@ -1,71 +1,6 @@
-// input element that used more than one time
+// *********************************************** helper functions ***********************************************
 const titleEl = document.getElementById("taskTitle");
 const descriptionEl = document.getElementById("taskDescription");
-
-// add event listener when reload page
-window.onload = function () {
-  saveInLocalStorage("existingTaskId", null);
-};
-
-// addTask and editTask
-function addTask(tasks) {
-  const taskId = getFromLocalStorage("existingTaskId");
-
-  //   task exist
-  if (taskId) {
-    console.log("run if");
-    const title = titleEl.value;
-    const description = descriptionEl.value;
-    // if title is empty
-    if (title.trim() === "") return;
-
-    const task = tasks.find((task) => task.id === Number(taskId));
-    task.title = title;
-    task.description = description;
-    // if we want to change completed
-    // task.completed = false;
-    saveInLocalStorage("existingTaskId", null);
-
-    // or
-    // localStorage.removeItem("existingTaskId");
-    saveInLocalStorage("tasks", tasks);
-  } else {
-    console.log("run else");
-    const title = titleEl.value;
-    const description = descriptionEl.value;
-    if (title.trim() === "") return;
-    tasks.push({
-      id: Date.now(),
-      title,
-      description,
-      completed: false,
-    });
-    saveInLocalStorage("tasks", tasks);
-  }
-  titleEl.value = "";
-  descriptionEl.value = "";
-}
-
-function toggleComplete(task, tasks) {
-  //   const task = tasks.find((task) => task.id === taskId);
-  task.completed = !task.completed;
-  saveInLocalStorage("tasks", tasks);
-}
-
-function editTask(task, tasks) {
-  //   const task = tasks.find((task) => task.id === taskId);
-  titleEl.value = task.title;
-  descriptionEl.value = task.description;
-  saveInLocalStorage("existingTaskId", task.id);
-}
-
-function deleteTask(taskId, tasks) {
-  tasks = tasks.filter((task) => task.id !== taskId);
-  saveInLocalStorage("tasks", tasks);
-  saveInLocalStorage("existingTaskId", null);
-  // refresh page
-  location.reload();
-}
 
 // *********************************************** helper functions ***********************************************
 const saveInLocalStorage = (key, value) => {
@@ -81,3 +16,62 @@ const getFromLocalStorage = (key) => {
   // convert string that came from local storage to its type
   return value ? JSON.parse(value) : null;
 };
+
+// *********************************************** Alpine.js logic ***********************************************
+document.addEventListener("alpine:init", () => {
+  Alpine.data("tasksManager", () => ({
+    tasks: JSON.parse(localStorage.getItem("tasks")) || [],
+    addTask() {
+      // .............
+      const taskId = getFromLocalStorage("existingTaskId");
+      //   task exist
+      if (taskId) {
+        const title = titleEl.value;
+        const description = descriptionEl.value;
+        // if title is empty
+        if (title.trim() === "") return;
+
+        const task = this.tasks.find((task) => task.id === Number(taskId));
+        task.title = title;
+        task.description = description;
+        // if we want to change completed
+        // task.completed = false;
+        saveInLocalStorage("existingTaskId", null);
+
+        // or
+        // localStorage.removeItem("existingTaskId");
+        saveInLocalStorage("tasks", this.tasks);
+      } else {
+        const title = titleEl.value;
+        const description = descriptionEl.value;
+        if (title.trim() === "") return;
+        this.tasks.push({
+          id: Date.now(),
+          title,
+          description,
+          completed: false,
+        });
+        saveInLocalStorage("tasks", this.tasks);
+      }
+      titleEl.value = "";
+      descriptionEl.value = "";
+    },
+    // ..............
+    toggleComplete(task) {
+      //   const task = tasks.find((task) => task.id === taskId);
+      task.completed = !task.completed;
+      saveInLocalStorage("tasks", this.tasks);
+    },
+    editTask(task) {
+      //   const task = tasks.find((task) => task.id === taskId);
+      titleEl.value = task.title;
+      descriptionEl.value = task.description;
+      saveInLocalStorage("existingTaskId", task.id);
+    },
+    deleteTask(taskId) {
+      this.tasks = this.tasks.filter((task) => task.id !== taskId);
+      saveInLocalStorage("tasks", this.tasks);
+      saveInLocalStorage("existingTaskId", null);
+    },
+  }));
+});
